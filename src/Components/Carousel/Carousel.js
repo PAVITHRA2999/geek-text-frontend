f5d182c6dabe825d97c5d31fc4f1badad6ec9174
LeanetAlfonso <leanet.alfonso1@gmail.com>
2022-09-16 17:37:08 -0400

    Fix -webkit-fill-available build error

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -37,7 +37,7 @@ const Carousel = ({ children, handleUpdateIndexCallback, offset, totalLength, au
 
     const nextIndex = () => {
         let newIndex;
-        if (activeIndex === totalLength - offset) {
+        if (activeIndex >= totalLength - offset) {
             newIndex = 0;
         }
         else {
--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -18,6 +18,11 @@ const HomeScreen = () => {
 		const newIndex = newIdx;
 		setActiveIndex(newIndex);
 	};
+	// We listen to the resize event
+	window.addEventListener('resize', () => {
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	});
 
 	const autobiography = useRef(null);
 	const fantasy = useRef(null);
