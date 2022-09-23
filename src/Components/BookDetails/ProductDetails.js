62ca0386f268814ad9cd60fd9a17a956a227c229
LeanetAlfonso <leanet.alfonso1@gmail.com>
2022-09-23 19:04:18 -0400

    Add Publication Date to book details

--- src/Components/BookDetails/ProductDetails.js
+++ src/Components/BookDetails/ProductDetails.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-const ProductDetails = ({ publisher, isbn, edition, genre }) => {
+const ProductDetails = ({ publisher, publicationDate, isbn, edition, genre }) => {
     return (
         <div className="details_block text_body">
             <div>
                 <div>Publisher:</div>
+                <div>Publication Date:</div>
                 <div>ISBN:</div>
                 <div>Edition:</div>
                 <div>Genre:</div>
             </div>
             <div>
                 <div>{publisher}</div>
+                <div>{publicationDate}</div>
+
                 <div>{isbn}</div>
                 <div>{edition}</div>
                 <div>{genre}</div>
