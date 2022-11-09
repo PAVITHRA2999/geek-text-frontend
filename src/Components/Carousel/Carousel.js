c096c826360e39651ca8211f3ba54c5019eca7c7
LeanetAlfonso <leanet.alfonso1@gmail.com>
2022-11-08 20:23:16 -0500

    Update home page carousel

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
-import { useSwipeable } from "react-swipeable";
-
 
 export const CarouselItem = ({ children, width }) => {
     return (
@@ -10,30 +8,10 @@ export const CarouselItem = ({ children, width }) => {
         </div>
     );
 };
-const Carousel = ({ children, handleUpdateIndexCallback, offset, totalLength, auto }) => {
+
+const Carousel = ({ children, handleUpdateIndexCallback, offset, totalLength, auto, slides }) => {
     const [activeIndex, setActiveIndex] = useState(0);
-    const items = React.Children;
-    const [paused, setPaused] = useState(false);
-
-    const handlers = useSwipeable({
-        onSwipedLeft: () => nextIndex(),
-        onSwipedRight: () => prevIndex()
-    });
-
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (!paused && auto) {
-                nextIndex();
-            }
-        }, 1500);
-
-        return () => {
-            if (interval) {
-                clearInterval(interval);
-            }
-        };
-    });
+    const items = slides || React.Children;
 
     const nextIndex = () => {
         let newIndex;
@@ -64,27 +42,47 @@ const Carousel = ({ children, handleUpdateIndexCallback, offset, totalLength, au
         <div className="carousel-container">
             {
                 (totalLength > offset || activeIndex !== 0) &&
-                <i className="fa-solid fa fa-chevron-left fa-lg indicator" disabled={activeIndex === 0} onClick={prevIndex}></i>
+                <i className="fa-solid fa fa-chevron-left fa-lg indicator"
+                    disabled={activeIndex === 0}
+                    onClick={prevIndex}></i>
             }
-            <div  {...handlers}
-                className="carousel"
-                onMouseEnter={() => setPaused(true)}
-                onMouseLeave={() => setPaused(false)}
-            >
-                <div className="inner" style={{ transform: `translateX(-${(activeIndex) * 100 / offset}%)` }}>
-                    {items.map(children, (child) => {
-                        return React.cloneElement(child, {
-                            width: "100%"
-                        });
-                    })}
+            <div className={`carousel ${auto && "auto-scroll"}`}>
+                <div
+                    className={`inner ${auto && "auto-scroll-inner"}`}
+                    style={{ transform: `translateX(-${(activeIndex) * 100 / offset}%)` }}>
+                    {!auto ?
+                        <div className={`${auto && "half-scroll"}`}>
+                            {items.map(children, (child) => {
+                                return React.cloneElement(child, {
+                                    width: "100%"
+                                });
+                            })
+                            }
+                        </div> :
+                        <>
+                            <div className="half-scroll">
+                                {items.map(item => {
+                                    return item;
+                                })
+                                }
+                            </div>
+                            <div className="half-scroll">
+                                {items.map(item => {
+                                    return item;
+                                })
+                                }
+                            </div>
+                        </>
+                    }
                 </div>
-
             </div>
             {
                 ((totalLength > offset) || (activeIndex < totalLength - offset)) &&
                 <i className="fa-solid fa fa-chevron-right fa-lg indicator"
                     disabled={activeIndex >= totalLength - offset}
-                    onClick={nextIndex}></i>}
+                    onClick={nextIndex}>
+                </i>
+            }
         </div>
     );
 };
